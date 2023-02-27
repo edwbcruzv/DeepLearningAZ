fc15945a88e305c3dd98d30ce2587039a1e62451
cruz <edwin.cruz.v@outlook.com>
2023-02-27 01:21:18 -0600

    V2

--- 1-Artificial_Neural_Networks(ANN)/ann.py
+++ 1-Artificial_Neural_Networks(ANN)/ann.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-Date: Wed Feb 22 22:39:14 2023
+Date: Wed Feb 22 20:15:46 2023
 
 @author: edwin
 """
-
 # =============================================================================
-# Plantilla de Clasificacion
+# Redes Neuronales Artificiales
 # =============================================================================
 # =============================================================================
 # --------------------Importando librerias--------------------
@@ -18,21 +17,23 @@ import pandas as pd
 # --------------------Importando dataset--------------------
 # =============================================================================
 
-# Estructura de los datos: {explicar eldataset y el objetivo}.
+# Estructura de los datos: Clientes de un banco.
+# Objetivo: analizar si despues de 6 meses un cliente se queda en el banco.
 # Filas :{numero de filas}
 # Columnas:
-#           |{col1}|{col2}|{...} (vars independiente)
-#           |{columna de var indep.}| (var_dependiente)
+#           |{...}| (vars independiente)
+#           |Exited| (var_dependiente)
 
-dataset = pd.read_csv('dataset.csv') # {buscar el dataset}
+dataset = pd.read_csv('Churn_Modelling.csv') # {buscar el dataset}
 
 # Variable independiente:Mayuscula por ser una matriz.
-#   tomamos [Todas las filas ,Solo la columna(s)...]
-X = dataset.iloc[:,1:2].values # {se pueden modificar segun se necesite}
+#   tomamos [Todas las filas ,desde la 4ta columna hasta la penultima]
+# el resto son datos irrelevantes
+X = dataset.iloc[:,3:-1].values 
 
 # Variable dependiente:minuscula por ser un vector.
 #   tomamos [Todas las filas: Solo la ultima columna]
-y = dataset.iloc[:,2:3].values # {se pueden modificar segun se necesite}
+y = dataset.iloc[:,[13]].values
 
 # Nota: convertir a matrices tanto a X como a y para evitar problemas
 #       al no usar matrices.
@@ -40,15 +41,15 @@ y = dataset.iloc[:,2:3].values # {se pueden modificar segun se necesite}
 # --------------------Tratamiendo de NAs--------------------
 # =============================================================================
 
-from sklearn.impute import SimpleImputer
+# from sklearn.impute import SimpleImputer
 # Los valores desconocidos de los valores independientes son los NA´s.
 # El valor que se va a sustituir que sera la media.
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+# imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 # Seleccionando las columnas las cuales estan los valores NA´s.
 # [Todas las filas,Columnas 1 y 2]
-imputer = imputer.fit(X[:,1:3]) # ajustando valores
+# imputer = imputer.fit(X[:,1:3]) # ajustando valores
 # Sobreescribirnedo la matriz con la nueva trasformacion configurada.
-X[:,1:3] = imputer.transform(X[:,1:3])
+# X[:,1:3] = imputer.transform(X[:,1:3])
 
 
 # =============================================================================
@@ -56,17 +57,19 @@ X[:,1:3] = imputer.transform(X[:,1:3])
 # =============================================================================
 # Los datos son categorias que se deben de tranformar a numeros para
 # que python los pueda trabajar.
-# En este caso las columnas "Country" y "Purchased".
+# En este caso las columnas "Geography" y "Gender".
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 
 # la funcion se encargara de transformar las categorias a datos numericos.
-le_X =LabelEncoder()
+le_X1 =LabelEncoder()
+le_X2 =LabelEncoder()
 # De la tabla de variables independientes se toma 
 # la columna "Country"y todas las filas. Y se sobreescribe la tabla.
-X[:,0]=le_X.fit_transform(X[:,0])
+X[:,1]=le_X1.fit_transform(X[:,1])
+X[:,2]=le_X2.fit_transform(X[:,2])
 
-# Ahora se debe de transformar la columna "Country" a variables dummy,
+# Ahora se debe de transformar la columnas categoricas a variables dummy,
 # creando una columna por cada categoria y de las variables dummy solo se
 # marca la categoria correcta con un booleano.
 
@@ -74,18 +77,14 @@ X[:,0]=le_X.fit_transform(X[:,0])
 ct = ColumnTransformer(
     # Lista de tuplas (nombre,transformador,columnas) que se le aplicara 
     # al conjunto de datos.
-    [('one_hot_encoder',OneHotEncoder(categories='auto',dtype=int),[0])],
+    [('one_hot_encoder',OneHotEncoder(categories='auto',dtype=int),[1])],
     # Se pasa el resto de columnas que no se tocaron.
     remainder='passthrough')
-# # Se creara una columna por cada pais (categorizando)
-# # Y con un booleano se sabra el pais correspondiente (onehotencoder)
-# X=ct.fit_transform(X)
-X = np.array(ct.fit_transform(X),dtype=float)
 
-# Se categoriza columna "Puerchased" usando solo o y 1, ya que solo son 
-# respuestas de YES o NO, no seran necesarios serararlos en 2 columnas. 
-le_y=LabelEncoder()
-y=le_y.fit_transform(y)
+X = np.array(ct.fit_transform(X),dtype=float)
+X=X[:,1:] # Se elimina la columna de Francia (multicolinealidad)
+
+
 
 # =============================================================================
 # --------------------Dividiendo dataset en conjuntos--------------------
@@ -118,6 +117,16 @@ X_test = sc_X.transform(X_test)
 ## pero en otras ocaciones si se necesitaran escalar
 
 # =============================================================================
+# Construyendo la Red Neuronal Artificial
+# =============================================================================
+import keras 
+
+# =============================================================================
+# Evaluar el modelo y calcular predicciones finales
+# =============================================================================
+
+
+# =============================================================================
 # Ajustar el modelo {modelo de clasificacion} al conjunto de entrenamiento
 # =============================================================================
 from sklearn import 
@@ -148,32 +157,3 @@ y_pred=classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 
 c_m=confusion_matrix(y_test, y_pred)
-
-# =============================================================================
-# Representacion grafica de los resultados del algoritmo
-# =============================================================================
-
-from matplotlib.colors import ListedColormap
-X_set, y_set=X_train, y_train
-
-# Genera todos los punto del dominio posible (mallado de la region)
-X1,X2=np.meshgrid(np.arange(start=X_set[:,0].min()-1,stop=X_set[:,0].max()+1,step=0.01),
-                  np.arange(start=X_set[:,1].min()-1,stop=X_set[:,1].max()+1,step=0.01))
-# pindando todo el plano 
-plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75,cmap=ListedColormap(('red','green')))
-# division de la region del trabajo (Dominio)
-plt.xlim(X1.min(),X1.max())
-plt.ylim(X2.min(),X2.max())
-
-# pintando los resultados categoricos en el grafico
-for i, j in enumerate(np.unique(y_set)):
-    aux=(y_set == j).ravel()
-    plt.scatter(X_set[aux, 0], X_set[aux, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-
-plt.title('Clasificador{}')
-plt.xlabel('algo en x')
-plt.ylabel('Si/No')
-plt.legend()
-plt.show()
